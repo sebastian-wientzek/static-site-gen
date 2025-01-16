2ce2cee9959a19372538790846fd0032229159cf
Sebastian Wientzek <6043437+sebastian-wientzek@users.noreply.github.com>
2025-01-16 18:24:01 +0100

    added enum TextType and class TextNode

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
+from textnode import TextNode, TextType
+
+def main():
+	textnode = TextNode("test", TextType.BOLD, "https://hi.com")
+	print(textnode)
+
+
 if __name__ == "__main__":
-	print("Hello, World!")
+	main()
